4ef5d6555bb23206453df609884c9ac0113ac045
RAGHURAM SADINENI <sadineni.raghuram@gmail.com>
2024-11-11 19:05:31 -0600

    WIP:SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 
+import robotsTxt from "astro-robots-txt";
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
+import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'server',
+  integrations: [tailwind(), robotsTxt({
+    policy: [{
+      userAgent: '*',
+      disallow: ''
+    }]
+  }), sitemap(), compress()],
+  image: {
+    service: passthroughImageService(),
+  },
+  output: 'hybrid',
   adapter: vercel(),
   i18n: {
     defaultLocale: "it",
